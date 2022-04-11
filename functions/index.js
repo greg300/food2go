8b8f23e44adefc1eba0d15fe077dabe785941ed8
Nicholas Meegan <74822467+Nyquistic1@users.noreply.github.com>
2022-04-10 21:16:18 -0400

    New APIs and curl examples

--- functions/index.js
+++ functions/index.js
@@ -44,7 +44,7 @@ app.post('/api/create/orders', (req, res) => {
       })();
   });
 
-
+  
 // Create new Employee.
 app.post('/api/create/employees', (req, res) => {
     console.log(req.body);
@@ -66,6 +66,43 @@ app.post('/api/create/employees', (req, res) => {
       })();
   });
 
+// Create new Customer.
+app.post('/api/create/customers', (req, res) => {
+    console.log(req.body);
+    (async () => {
+        try {
+            db.ref('customers/' + req.body['username']).set( {
+                password: req.body['password'],
+                email: req.body['email'],
+                name: req.body['name'],
+                phone: req.body['phone']
+            });
+            return res.status(200).send();
+        } catch (error) {
+            console.log(error);
+            return res.status(500).send(error);
+        }
+      })();
+  });
+
+// Create new Food item.
+app.post('/api/create/foods', (req, res) => {
+  console.log(req.body);
+  (async () => {
+      try {
+          db.ref('foods/' + req.body['food_item_id']).set( {
+              name: req.body['name'],
+              description: req.body['description'],
+              price: req.body['price']
+          });
+          return res.status(200).send();
+      } catch (error) {
+          console.log(error);
+          return res.status(500).send(error);
+      }
+    })();
+});
+
 
 // --- READ ---
 // Read all Orders.
@@ -76,6 +113,125 @@ app.get('/api/read/orders', (req, res) => {
             db.ref('orders/').once('value').then(function(snapshot) {
                 console.log(snapshot.val())
             })
+            return res.status(200).send();
+        } catch (error) {
+            console.log(error);
+            return res.status(500).send(error);
+        }
+      })();
+  });
+
+// Read all Food items.
+app.get('/api/read/foods', (req, res) => {
+  console.log(req.body);
+  (async () => {
+      try {
+          db.ref('foods/').once('value').then(function(snapshot) {
+              console.log(snapshot.val())
+          })
+          return res.status(200).send();
+      } catch (error) {
+          console.log(error);
+          return res.status(500).send(error);
+      }
+    })();
+});
+
+// --- UPDATE ---
+// Update Employee.
+app.patch('/api/update/employees/:employee_id', (req, res) => {
+    var employee_id = req.param("employee_id");
+    console.log(employee_id);
+    (async () => {
+        try {
+            if(req.body['password']){
+              db.ref('employees/' + employee_id).update( {
+                password: req.body['password']});  
+            }
+            if(req.body['email']){
+              db.ref('employees/' + employee_id).update( {
+                email: req.body['email']});  
+            }
+            if(req.body['name']){
+              db.ref('employees/' + employee_id).update( {
+                name: req.body['name']});  
+            }
+            if(req.body['phone']){
+              db.ref('employees/' + employee_id).update( {
+                phone: req.body['phone']});  
+            }
+            if(req.body['position']){
+              db.ref('employees/' + employee_id).update( {
+                position: req.body['position']});  
+            }
+            return res.status(200).send();
+        } catch (error) {
+            console.log(error);
+            return res.status(500).send(error);
+        }
+      })();
+  });
+
+// Update Customer.
+app.patch('/api/update/customers/:username', (req, res) => {
+    var username = req.param("username");
+    console.log(username);
+    (async () => {
+        try {
+            if(req.body['password']){
+              db.ref('customers/' + username).update( {
+                password: req.body['password']});  
+            }
+            if(req.body['email']){
+              db.ref('customers/' + username).update( {
+                email: req.body['email']});  
+            }
+            if(req.body['name']){
+              db.ref('customers/' + username).update( {
+                name: req.body['name']});  
+            }
+            if(req.body['phone']){
+              db.ref('customers/' + username).update( {
+                phone: req.body['phone']});  
+            }
+            return res.status(200).send();
+        } catch (error) {
+            console.log(error);
+            return res.status(500).send(error);
+        }
+      })();
+  });
+
+// Add Food Item into cart.
+
+
+// Remove Food Item from cart.
+
+
+
+// --- DELETE ---
+// Delete Employee.
+app.delete('/api/delete/employees/:employee_id', (req, res) => {
+    var employee_id = req.param("employee_id");
+    console.log(employee_id);
+    (async () => {
+        try {
+            db.ref('employees/' + employee_id).remove();
+            return res.status(200).send();
+        } catch (error) {
+            console.log(error);
+            return res.status(500).send(error);
+        }
+      })();
+  });
+
+// Delete Customer.
+app.delete('/api/delete/customers/:username', (req, res) => {
+    var username = req.param("username");
+    console.log(username);
+    (async () => {
+        try {
+            db.ref('customers/' + username).remove();
             return res.status(200).send();
         } catch (error) {
             console.log(error);
