9a6829f36fd37d757b5e8c98aed1c1d9f14e869d
Nicholas Meegan <74822467+Nyquistic1@users.noreply.github.com>
2022-04-18 14:02:23 -0400

    Fixes to errors with db

--- public/app.js
+++ public/app.js
@@ -1,26 +1,3 @@
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCzSw2RqMAzxT8nxmzGbca_n-OaoSzSj5U",
-//   authDomain: "food2go-56aac.firebaseapp.com",
-//   databaseURL: "https://food2go-56aac-default-rtdb.firebaseio.com",
-//   projectId: "food2go-56aac",
-//   storageBucket: "food2go-56aac.appspot.com",
-//   messagingSenderId: "912146962405",
-//   appId: "1:912146962405:web:ba727c4ba777ab82ce410c",
-//   measurementId: "G-SMDE1KW84D"
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
-
 const dbRef = firebase.database().ref();
 const foodsRef = dbRef.child('foods');
 
